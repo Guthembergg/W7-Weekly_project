90e9233857d3059c1c7658d7b21feebb0900bfe9
Karim Guettech <karimguettech@gmail.com>
2023-01-27 16:57:22 +0100

    added alert

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+var array1 = [];
+var c = 0;
 const asyncWait = async function (url, where) {
   try {
     let res = await fetch(url);
@@ -9,6 +11,7 @@ const asyncWait = async function (url, where) {
       console.log(jarray);
       if (where === "favourite") {
         jarray.forEach((element) => {
+          array1.push(element);
           div.innerHTML += `
 
 
@@ -29,6 +32,8 @@ const asyncWait = async function (url, where) {
         });
       } else if (where === "carousel") {
         jarray.forEach((item, index) => {
+          array1.push(item);
+
           // Add items from the JSON file
           const carouselItem = document.createElement("div");
           carouselItem.classList.add("carousel-item");
@@ -46,6 +51,8 @@ const asyncWait = async function (url, where) {
         });
       } else {
         jarray.forEach((element) => {
+          array1.push(element);
+
           div.innerHTML += `
         <div class="card col-12 col-sm-5 col-lg-3 m-2 " id="${element.id}" >
         <img class="card-img-top" src="${element.album.cover_xl}" alt="Card image cap">
@@ -60,6 +67,37 @@ const asyncWait = async function (url, where) {
         `;
         });
       }
+
+      function shuffle() {
+        array1.sort((a, b) => a.rank - b.rank);
+        let alertDiv = document.getElementById("alertDiv");
+
+        if (c === 0) {
+          alertDiv.classList.remove("d-none");
+          array1.forEach((element, index) => {
+            let newAlert = document.createElement("div");
+            newAlert.classList.add("alert");
+            newAlert.role = "alert";
+
+            alertDiv.appendChild(newAlert);
+            newAlert.innerHTML = `${index + 1}: ${element.artist.name} - ${
+              element.title
+            }- Rank: ${element.rank}`;
+          });
+          c++;
+          // alert(JSON.stringify(array1));
+        } else {
+          alertDiv.classList.toggle("d-none");
+        }
+      }
+      function modalFunction() {
+        let modalDiv = document.getElementById("modalDiv");
+      }
+
+      let shuffleButton = document.getElementById("shuffleMe");
+      shuffleButton.onclick = shuffle;
+      let modalButton = document.getElementById("modal");
+      shuffleButton.onclick = modalFunction;
     }
   } catch (err) {
     console.log(err);
@@ -82,3 +120,4 @@ const thirdParagraph = asyncWait(
 
   "carousel"
 );
+console.log(array1);
